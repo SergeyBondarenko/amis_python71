9796d8fc4e6d812a5aee4ae9fab8a94fd1dd4967
Sergey Bondarenko <33322622+SergeyBondarenko@users.noreply.github.com>
2017-11-02 19:28:04 +0200

    homework_4
    тема "Конструкції керування"

---
+++ km71/Bondarenko_Sergii/4/task1.py
@@ -0,0 +1,8 @@
+first_num = float(input("Введите первое число:"))
+second_num = float(input("Введите второе число:"))
+if first_num > second_num:
+    num = second_num 
+else: 
+    num = first_num 
+print("Меньшим является число " + str(num))
+print(input("Нажмите клавишу \"Enter\" для окончания работы программы"))
